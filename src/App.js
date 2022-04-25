9d9dc6cf21e2f7270742d1ef2d2c53c34ef679e6
kmieciu6 <jakub.kmiecik96@gmail.com>
2022-04-25 13:23:27 +0200

    App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
     return (
         <HashRouter>
             <Routes>
+                <Route exact path={"/"} element={<Home/>} />
                 <Route exact path={"/Home"} element={<Home/>} />
                 <Route exact path={"/Login"} element={<Login/>} />
                 <Route exact path={"/SignUp"} element={<SignUp/>} />
